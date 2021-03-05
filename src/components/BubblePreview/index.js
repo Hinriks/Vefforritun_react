88c8897f3ccc24a85116f2606b4a4d391ae6e758
Patrekur Örn <patrekurf19@ru.is>
2021-03-05 15:46:11 +0000

    fix style of products

--- src/components/BubblePreview/index.js
+++ src/components/BubblePreview/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './style.css';
 
 const BubblePreview = ({ item }) => {
     return (
-        <div className="card mb-4 box-shadow">
+        <div className="card mb-4 box-shadow" style={styles}>
             <img className="card-img-top" src={item.image} alt={item.name}/>
             <div className="card-body">
                 <h5 className="card-title">{item.name}</h5>
                 <p className="card-text">{item.description}</p>
-                <a href="/" className="btn btn-primary float-right">Buy now!</a>
             </div>
+            <a href="/" className="btn btn-primary">Buy now!</a>
         </div>
     )
 };
