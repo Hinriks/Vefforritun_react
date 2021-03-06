55928d228104772714c6b7a21543cb22b90222f6
Patrekur Örn <patrekurf19@ru.is>
2021-03-06 17:57:14 +0000

    ..

--- src/components/BubblePreview/index.js
+++ src/components/BubblePreview/index.js
@@ -4,11 +4,15 @@ import styles from './style.css';
 const BubblePreview = ({ item }) => {
     return (
         <div className="card mb-4 box-shadow" style={styles}>
-            <img className="card-img-top" src={item.image} alt={item.name}/>
+            <a href={"/bubbles/" + item.id} className='image'>
+                <img className="card-img-top" src={item.image} alt={item.name}/>
+            </a>
             <div className="card-body">
-                <h5 className="card-title">{item.name}</h5>
+                <a href={"/bubbles/" + item.id}>
+                    <h5 className="card-title">{item.name}</h5>
+                </a>
                 <p className="card-text">{item.description}</p>
-                <a href={"/bubbles/" + item.id } className="btn btn-primary float-right">Buy now!</a>
+                {/*<a href={"/bubbles/" + item.id } className="btn btn-primary float-right">Buy now!</a>*/}
             </div>
             <a href="/" className="btn btn-primary">Buy now!</a>
         </div>
