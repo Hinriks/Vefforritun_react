55fc1d8bbed8e03809e7f7825d232c2355db4e12
Hinriks <hinrik19@ru.is>
2021-03-07 17:11:47 +0000

    .+

--- src/components/Bundles/index.js
+++ src/components/Bundles/index.js
@@ -14,7 +14,6 @@ const Bundles = () => {
         <div className='container'>
             <h1 className='title'>Bundles</h1>
             <div className="bundleContainer">
-                {console.log("bundles: " + Object.values(bundles))}
                 {Object.values(bundles).map(item =>
                     <div className="col-md-4" key={item.id}>
                         <h2 className='bundleName' style={styles}>{item.name}</h2>
--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,3 @@
 export const BUBBLES_URL = "http://localhost:3500/api/bubbles";
 export const BUNDLES_URL = "http://localhost:3500/api/bundles";
+export const ORDER_URL = "http://localhost:3500/api/orders";
